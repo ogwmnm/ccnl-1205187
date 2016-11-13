813d756a9dd090ed020de10286a2da7aa47c80c2
Minami Ogawa <ogwmnm@gmail.com>
2016-11-13 15:41:08 +0900

    Fix side bar position

--- js/main.js
+++ js/main.js
@@ -20,4 +20,52 @@
     $("body, html").animate({ scrollTop: 0 }, 500);
   });
 
+
+  var navbar = {},  $navbar = $("#js-navbar"),
+      header = {},  $header = $("#js-header"),
+      sideNav = {}, $sideNav = $("#js-side-nav"),
+
+      calc = function() {
+        navbar.height = $navbar.outerHeight()
+
+        header.height = $header.outerHeight();
+        header.bottom = $header.offset().top + $header.outerHeight();
+
+        sideNav.top = $sideNav.offset().top;
+        sideNav.left = $sideNav.offset().left;
+        sideNav.marginTop = sideNav.top - header.bottom;
+      };
+
+  // 各要素の位置・サイズを計算する
+  calc();
+
+  $window
+    .on("resize", function() {
+      // ウィンドウサイズが変わったら計算し直す
+      calc();
+    })
+    .on("scroll", function() {
+      if ($window.scrollTop() > header.height) {
+
+        // スクロール量がヘッダーの高さより大きくなったら
+        // サイドメニューの位置を固定する
+        $sideNav.css({
+          position: "fixed",
+          top: (sideNav.marginTop + navbar.height),
+          left: sideNav.left
+        });
+
+      } else {
+
+        // スクロール量がヘッダーの高さより小さくなったら
+        // サイドメニューの位置を元に戻す
+        $sideNav.css({
+          position: "relative",
+          top: 0,
+          left: 0
+        });
+
+      }
+    });
+
 })(jQuery);
